5b9920c176df02cb60dee6bc05526245f7348436
wangyouxiang-yyds <tree8454@gmail.com>
2025-02-18 12:12:59 +0800

    add float_type.c

--- Basic/Basic Data Type/float_type.c
+++ Basic/Basic Data Type/float_type.c
@@ -8,8 +8,13 @@ int main(int argc, char const *argv[])
 
     printf("num1 = %e\n", num1); // 以指數型態印出num1 的值
     printf("num2 = %f\n", num2); // 以符點數的型態印出num2的值
-    
 
+    // float vs double
+    float num3 = 123.456789012345F;
+    double num4 = 123.456789012345;
+
+    printf("num3 = %16.12f\n", num3);
+    printf("num4 = %16.12f\n", num4);
 
 
 
