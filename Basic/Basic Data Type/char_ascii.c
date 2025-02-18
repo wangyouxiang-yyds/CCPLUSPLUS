83edbfd49cd01dc4836f3535f7a10a7f3c293052
wangyouxiang-yyds <tree8454@gmail.com>
2025-02-18 11:46:45 +0800

    add 12 line

---
+++ Basic/Basic Data Type/char_ascii.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    // 字元常數必須放在單引號裡
+    char ch = 'a';
+    printf("ch = %c\n", ch);    // 印出ch 字串
+    printf("ASCII of ch = %d\n", ch);   // 印出ascii碼 10進位 97
+
+    char ninety = 90;
+    printf("ch=%c\n", ninety);
+
+
+
+    return 0;
+}
