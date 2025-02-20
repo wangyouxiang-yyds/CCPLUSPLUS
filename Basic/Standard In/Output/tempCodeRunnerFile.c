5ce66444b859921b0e0cbdc8f05bac5dbd624246
wangyouxiang-yyds <tree8454@gmail.com>
2025-02-20 14:15:18 +0800

    add create.c

---
+++ Basic/Standard In/Output/tempCodeRunnerFile.c
@@ -0,0 +1,17 @@
+int a, b;
+    // // 一般輸入
+    // scanf("%d %d", &a, &b);
+    // printf("%d + %d = %d\n", a, b, a + b);
+    
+    // // 如中間有逗號，輸入逗號來區隔
+    // scanf("%d,%d", &a, &b);
+    // printf("%d + %d = %d\n", a, b, a + b);
+
+    // // 輸入字元，需用%c
+    // char ch;
+    // scanf(" %c", &ch);  // 需刻意隔一個空白，否則會讀到空白而無法輸入
+    // printf("ch = %c , ascii code is %d\n", ch, ch);
+
+    // char name[10];  
+    // scanf("%s", name);  // 如果是陣列不需要加上&
+    // printf("Hi %s How are you?\n", name);
