beb0596e42eec0b8e601da09236d262c92218561
wangyouxiang-yyds <tree8454@gmail.com>
2025-02-21 15:45:57 +0800

    add a if_else filelist and add switch.c

---
+++ Basic/if_else/switch.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    char oper;
+    printf("please inpua a operator Ex(3+2)");
+    scanf("%d %c %d", &a, &oper, &b);
+
+    switch (oper)
+    {
+    case '+':
+        printf("%d + %d = %d\n", a, b, a + b);
+        break;
+    case '-':
+        printf("%d - %d = %d\n", a, b, a - b);
+        break;
+    case '*':
+        printf("%d * %d = %d\n", a, b, a * b);
+        break;
+    case '/':
+        printf("%d / %d = %d\n", a, b, a / b);
+        break;
+    
+    default:
+        printf("Sorry can't regornize the sentence");
+        break;
+    }
+    return 0;
+}
